460c437bde541893c5c3c2aba9022d96701d7f34
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-02-28 20:42:25 +0530

    chore: Changed API key

--- src/ts/util/config.ts
+++ src/ts/util/config.ts
@@ -4,4 +4,5 @@ export const TIMEOUT_SECS = 10;
 export const RESULTS_PER_PAGE = 10;
 export const iconPath = new URL("../../img/icons.svg", import.meta.url)
 	.pathname;
-export const API_KEY = "f4d2e3c3-1daa-45dd-89ce-76f93d6783ca";
+export const API_KEY = "11d2ae43-89ed-4b68-8f63-dcb741ad6a8b";
+
